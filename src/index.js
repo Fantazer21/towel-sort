ceec0ca20f51cacd57a04ac5bb8b1a3fb3084b75
Ilya21 <6967221@gmail.com>
2020-10-18 14:30:12 +0200

    init: solution towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+
+	// condition 1)matrix no params 2)matrix is empty
+  if (matrix === undefined || matrix.length === 0) {return [];}
+  		// create an empty array for adding result
+      let array = [];
+    for (let i = 0; i < matrix.length; i++) {
+     if (i % 2 === 0) { //counter for integer value
+        array = array.concat(matrix[i]);   // .concat add an array to empty array
+      } else { array = array.concat( matrix[i].reverse() ); // .concat add an array not an integers values to empty array
+       }
+    }
+    return array;
+ }
+      
+       
+        
+      
+    
+   
+  	
+   
+   
+
